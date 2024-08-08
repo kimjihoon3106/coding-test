319e5e3d66aedcb597fb0adf673d69633e4f1e59
kimjihoon3106 <s24021@gsm.hs.kr>
2024-08-08 11:04:08 +0900

    solve py

---
+++ python/11021py.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.read
+
+data = input().split()
+
+n = int(data[0])
+values = []
+
+index = 1
+for i in range(n):
+    a = int(data[index])
+    b = int(data[index + 1])
+    values.append(a + b)
+    index += 2
+
+for i, value in enumerate(values):
+    print("Case #" + str(i + 1) + ":", value)
