24bc128e1844850843b8652fa1ac7b1d161609f4
kimjihoon3106 <s24021@gsm.hs.kr>
2024-08-09 23:15:58 +0900

    diffrent style code

--- python/11021py.py
+++ python/11021py.py
@@ -15,3 +15,19 @@ for i in range(n):
 
 for i, value in enumerate(values):
     print("Case #" + str(i + 1) + ":", value)
+
+# 같은 코드
+
+'''
+n = int(input())
+
+values = []
+
+for i in range(n):
+    a, b = map(int, input().split())
+    values.append(a + b)
+
+for i, value in enumerate(values):
+    print("Case #" + str(i + 1) + ":", value)
+
+'''
